dd20b467bfe8bb2f988571cf62db31c277ce170b
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-12-15 00:06:09 +0200

    chore: Add test data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { User } from './types';
+
 function App() {
-  return (
+	const users: User[] = [
+		{
+			name: 'Salah',
+			messages: [
+				{
+					body: 'Hello World!',
+				},
+				{
+					body: 'How are you?',
+				},
+			],
+		},
+	];
+
+	return (
 		<div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12">
 			<h2 className="text-4xl text-yellow-500">Hello World!</h2>
 		</div>

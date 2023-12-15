dadfdb25f77285a2ddaa3d985e0a2a0e14bb9709
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-12-15 17:14:44 +0200

    chore: Implement UserDisplay component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import UserDisplay from './components/UserDisplay';
 import { User } from './types';
 
 function App() {
@@ -17,7 +18,9 @@ function App() {
 
 	return (
 		<div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12">
-			<h2 className="text-4xl text-yellow-500">Hello World!</h2>
+			{users.map((user, i) => (
+				<UserDisplay user={user} key={i} />
+			))}
 		</div>
 	);
 }

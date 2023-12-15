8ec8959fe29f8706e4c19908b7f75aed37f68ed8
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-12-15 17:19:00 +0200

    fix: Use user's id as key instead of index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,15 @@ import { User } from './types';
 function App() {
 	const users: User[] = [
 		{
+			id: 1,
 			name: 'Salah',
 			messages: [
 				{
+					id: 1,
 					body: 'Hello World!',
 				},
 				{
+					id: 2,
 					body: 'How are you?',
 				},
 			],
@@ -18,8 +21,8 @@ function App() {
 
 	return (
 		<div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12">
-			{users.map((user, i) => (
-				<UserDisplay user={user} key={i} />
+			{users.map((user) => (
+				<UserDisplay user={user} key={user.id} />
 			))}
 		</div>
 	);

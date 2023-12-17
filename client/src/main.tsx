30a72b12afa3db0bb2acb3663178b1348f486c49
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-12-17 04:44:45 +0200

    chore: Add urql package and configure urql client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createClient, Provider } from 'urql';
 import App from './App.tsx';
 import './index.css';
 
+const client = createClient({
+  url: 'http://0.0.0.0:4000/graphql',
+  exchanges: [],
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
